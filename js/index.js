f8075476ca9713d20cf18cdf6d5e296c397ff4ef
Raul Luna <rluna@go-labs.net>
2016-07-04 15:39:29 -0600

    android push notifications

--- js/index.js
+++ js/index.js
@@ -16,34 +16,88 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+ var app = {
+  // Application Constructor
+  initialize: function() {
+    this.bindEvents();
+  },
+  // Bind Event Listeners
+  //
+  // Bind any events that are required on startup. Common events are:
+  // 'load', 'deviceready', 'offline', and 'online'.
+  bindEvents: function() {
+    document.addEventListener('deviceready', this.onDeviceReady, false);
+  },
+  // deviceready Event Handler
+  //
+  // The scope of 'this' is the event. In order to call the 'receivedEvent'
+  // function, we must explicitly call 'app.receivedEvent(...);'
+  onDeviceReady: function() {
+    app.receivedEvent('deviceready');
+  },
+  // Update DOM on a Received Event
+  receivedEvent: function(id) {
+    try {
+      var idnotify = localStorage.idnotify;
+      if (idnotify == null || idnotify == "" || idnotify == undefined){
+        var pushNotification = window.plugins.pushNotification;
+        pushNotification.register(this.successHandler,this.errorHandler,{"senderID":"804132887754","ecb":"app.onNotificationGCM"});
+      }
+    }catch(err) {
     }
+    var parentElement = document.getElementById(id);
+    var listeningElement = parentElement.querySelector('.listening');
+    var receivedElement = parentElement.querySelector('.received');
+
+    listeningElement.setAttribute('style', 'display:none;');
+    receivedElement.setAttribute('style', 'display:block;');
+
+    console.log('Received Event: ' + id);
+  },
+  successHandler: function(e) {
+  },
+  errorHandler: function(e) {
+  },
+  onNotificationGCM: function(e) {
+    switch( e.event ) {
+      case 'registered':
+      try {
+        if ( e.regid.length > 0 ) {
+          localStorage.idnotify = e.regid;
+          var notification = localStorage.notify;
+          if (notification == null || notification == "" || notification == 'si' || notification == undefined){
+            var request = new XMLHttpRequest();
+            request.open("GET", "http://www.wai-news.com/index.php?option=com_jbackend&view=request&action=put&module=push&resource=register&token=" + e.regid + "&appcode=nms.wai.001&platform=android",
+              true);
+            request.send();
+          }
+        }
+      }catch(err) {
+      }
+      break;
+      case 'message':
+      try {
+        var pushNotification = window.plugins.pushNotification;
+        var title = '';
+        var idioma = localStorage.idioma;
+        if (idioma == 'es'){
+          title = 'Notificación';
+        } else if (idioma == 'pt'){
+          title = 'Notificação';
+        } else {
+          title = 'Notification';
+        }
+        navigator.notification.alert(e.payload.message, null, e.payload.title != '' ? e.payload.title : title, 'OK');
+        if (e.payload.sound) {
+          var snd = new Media(e.payload.sound);
+          snd.play();
+        }
+        if (e.payload.badge){
+          pushNotification.setApplicationIconBadgeNumber(this.successBadge, this.errorHandler, e.payload.badge);
+        }
+      }catch(err) {
+      }
+      break;
+    }
+  }
 };
